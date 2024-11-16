b9a3efd80c9b641f19b13ae9de817df71246f66d
kiran <imkiran13@gmail.com>
2024-11-16 19:59:37 +0530

    files added

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
-const appName = process.env.APP_NAME
+const appName = process.env.APP_NAME || "NodeApp";
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
-app.use('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-    console.log(`Request served by ${appName}`);
+app.use("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+  console.log(`Request served by ${appName}`);
 });
 
 app.listen(port, () => {
-    console.log(`${appName} is listening on port ${port}`);
+  console.log(`${appName} is listening on port ${port}`);
 });
